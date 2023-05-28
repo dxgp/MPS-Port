50e6025c3f3dcbc6cec404b5e11b80312151e86c
dxgp_ <gunjanpayal@icloud.com>
2023-05-28 12:21:48 +0530

    private header

---
+++ MetalPerformanceShaders/MPSHeaderBridge.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "MPSPrivate.hpp"
+
+namespace MPS::Private::Class
+{
+    // _MTL_PRIVATE_DEF_CLS(MTLAccelerationStructureBoundingBoxGeometryDescriptor);
+}
+
+namespace MPS::Private::Protocol
+{
+    // _MTL_PRIVATE_DEF_PRO(MTLAccelerationStructure);
+}
+
+namespace MPS::Private::Selector
+{
+    // _MTL_PRIVATE_DEF_SEL(GPUEndTime,"GPUEndTime");
+}
---
+++ MetalPerformanceShaders/MetalPerformanceShaders.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "MPSDefines.hpp"
+#include "MPSHeaderBridge.hpp"
+#include "MPSPrivate.hpp"
