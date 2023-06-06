c8e81ce0e3d61a959ee5145f2524795d0543a1c2
dxgp_ <gunjanpayal@icloud.com>
2023-06-06 18:37:56 +0530

    rect mat test not working

--- MPSMatrixDecomposition_test.cpp
+++ MPSMatrixDecomposition_test.cpp
@@ -41,20 +41,17 @@ bool verifyDecompositionResult(MTL::Buffer* buffer, float* matrix_val){
 
 TEST_CASE("Testing LU Decomposition"){
     SUBCASE("Square Matrix Decomposition (5x5)"){
+        srand(50);
         rand();
         rand();
-
 
         std::vector<float> data;
-
-        // sourceMatrix = Eigen::Map<Eigen::MatrixXf>(&data[0], 3, 3).transpose();
         MPS::Matrix* source_mat = MPS::Matrix::alloc();source_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 5, 20, MPS::MPSDataTypeFloat32));
         MPS::Matrix* result_mat = MPS::Matrix::alloc();result_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 5, 20, MPS::MPSDataTypeFloat32));
         MPS::Matrix* pivot_indices = MPS::Matrix::alloc();pivot_indices->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(1, 5, 20, MPS::MPSDataTypeUInt32));
         MTL::Buffer* buf = device->newBuffer(4, MTL::ResourceStorageModeShared);
 
         //random data generation
-
         generateRandomFloatData(source_mat->data(), data);
 
         //eigen part
@@ -76,10 +73,45 @@ TEST_CASE("Testing LU Decomposition"){
         cmdBuffer->waitUntilCompleted();
 
         Eigen::MatrixXf eigen_result = (upperTriangularMatrix + lowerTriangularMatrix).transpose();
+
         CHECK(verifyDecompositionResult(result_mat->data(), eigen_result.data()));
     }
-    SUBCASE("Rectangular Matrix Decomposition"){
+    SUBCASE("Rectangular Matrix Decomposition (5x3)"){
 
+        std::vector<float> data;
+        MPS::Matrix* source_mat = MPS::Matrix::alloc();source_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 3, 12, MPS::MPSDataTypeFloat32));
+        MPS::Matrix* result_mat = MPS::Matrix::alloc();result_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 3, 12, MPS::MPSDataTypeFloat32));
+        MPS::Matrix* pivot_indices = MPS::Matrix::alloc();pivot_indices->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(1, 3, 12, MPS::MPSDataTypeUInt32));
+        MTL::Buffer* buf = device->newBuffer(4, MTL::ResourceStorageModeShared);
+
+        //random data generation
+        generateRandomFloatData(source_mat->data(), data);
+        
+        //eigen part
+        Eigen::MatrixXf sourceMatrix = Eigen::Map<Eigen::MatrixXf>(data.data(), 5, 3).transpose();
+        Eigen::FullPivLU<Eigen::MatrixXf> luDecomp(sourceMatrix);
+        Eigen::MatrixXf lowerTriangularMatrix = luDecomp.matrixLU().triangularView<Eigen::Lower>();
+        Eigen::MatrixXf upperTriangularMatrix = luDecomp.matrixLU().triangularView<Eigen::StrictlyUpper>();
+        Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic> permutationMatrix = luDecomp.permutationP();
+        Eigen::VectorXi permutationIndices = permutationMatrix.indices();
+
+        std::cout<<"REACHED"<<std::endl;
+        // metal part
+        MTL::CommandQueue* cmdQueue = device->newCommandQueue();
+        MTL::CommandBuffer* cmdBuffer = cmdQueue->commandBuffer();
+        MPS::MatrixDecompositionLU* lu_decomp = MPS::MatrixDecompositionLU::alloc();
+        lu_decomp->initWithDevice(device, 5, 3);
+        lu_decomp->encodeToCommandBuffer(cmdBuffer, source_mat, result_mat, pivot_indices, buf);
+        cmdBuffer->commit();
+        cmdBuffer->waitUntilCompleted();
+
+        Eigen::MatrixXf eigen_result = (upperTriangularMatrix + lowerTriangularMatrix).transpose();
+
+        std::cout<<eigen_result<<std::endl;
+        printMTLBuffer(result_mat->data(), "Result");
+        std::cout<<"MTL Code return: "<<(((MPS::MatrixDecompositionStatus *) buf->contents())[0])<<std::endl;
+        printMTLBuffer(source_mat->data(), "input");
+        CHECK(verifyDecompositionResult(result_mat->data(), eigen_result.data()));
     }
 }
 TEST_CASE("Testing Cholesky Decomposition"){
