d307ba1fffcbafa5a65f730748fe74434cf3e69f
dxgp_ <gunjanpayal@icloud.com>
2023-06-13 08:21:49 +0530

    working on testing

--- tests/MPSMatrixDecomposition_test.cpp
+++ tests/MPSMatrixDecomposition_test.cpp
@@ -25,6 +25,23 @@ bool verifyDecompositionResult(MTL::Buffer* buffer, float* matrix_val){
     }
     return true;
 }
+bool compareBuffers(MTL::Buffer* b1, MTL::Buffer* b2){
+    float* b1_vals = (float*)b1->contents();
+    float* b2_vals = (float*)b2->contents();
+    for(int i=0; i<b1->length()/4;i++){
+        if(!areEqual(b1_vals[i], b2_vals[i])){
+            return false;
+        }
+    }
+    return true;
+}
+void removeElementsAboveDiagonal(float* matrix, int rows, int columns) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = i + 1; j < columns; j++) {
+            matrix[i * columns + j] = 0.0;
+        }
+    }
+}
 
 TEST_CASE("Testing LU Decomposition"){
     SUBCASE("Square Matrix Decomposition (5x5)"){
@@ -63,49 +80,41 @@ TEST_CASE("Testing LU Decomposition"){
 
         CHECK(verifyDecompositionResult(result_mat->data(), eigen_result.data()));
     }
-    SUBCASE("Rectangular Matrix Decomposition (5x3)"){
-
-        std::vector<float> data;
-        MPS::Matrix* source_mat = MPS::Matrix::alloc();source_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 3, 12, MPS::MPSDataTypeFloat32));
-        MPS::Matrix* result_mat = MPS::Matrix::alloc();result_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 3, 12, MPS::MPSDataTypeFloat32));
-        MPS::Matrix* pivot_indices = MPS::Matrix::alloc();pivot_indices->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(1, 3, 12, MPS::MPSDataTypeUInt32));
-        MTL::Buffer* buf = device->newBuffer(4, MTL::ResourceStorageModeShared);
-
-        //random data generation
-        generateRandomFloatData(source_mat->data(), data);
-        
-        //eigen part
-        Eigen::MatrixXf sourceMatrix = Eigen::Map<Eigen::MatrixXf>(data.data(), 5, 3).transpose();
-        Eigen::FullPivLU<Eigen::MatrixXf> luDecomp(sourceMatrix);
-        Eigen::MatrixXf lowerTriangularMatrix = luDecomp.matrixLU().triangularView<Eigen::Lower>();
-        Eigen::MatrixXf upperTriangularMatrix = luDecomp.matrixLU().triangularView<Eigen::StrictlyUpper>();
-        Eigen::PermutationMatrix<Eigen::Dynamic, Eigen::Dynamic> permutationMatrix = luDecomp.permutationP();
-        Eigen::VectorXi permutationIndices = permutationMatrix.indices();
-
-        std::cout<<"REACHED"<<std::endl;
-        // metal part
-        MTL::CommandQueue* cmdQueue = device->newCommandQueue();
-        MTL::CommandBuffer* cmdBuffer = cmdQueue->commandBuffer();
-        MPS::MatrixDecompositionLU* lu_decomp = MPS::MatrixDecompositionLU::alloc();
-        lu_decomp->initWithDevice(device, 5, 3);
-        lu_decomp->encodeToCommandBuffer(cmdBuffer, source_mat, result_mat, pivot_indices, buf);
-        cmdBuffer->commit();
-        cmdBuffer->waitUntilCompleted();
-
-        Eigen::MatrixXf eigen_result = (upperTriangularMatrix + lowerTriangularMatrix).transpose();
-
-        std::cout<<eigen_result<<std::endl;
-        printMTLBuffer(result_mat->data(), "Result");
-        std::cout<<"MTL Code return: "<<(((MPS::MatrixDecompositionStatus *) buf->contents())[0])<<std::endl;
-        printMTLBuffer(source_mat->data(), "input");
-        CHECK(verifyDecompositionResult(result_mat->data(), eigen_result.data()));
-    }
 }
 TEST_CASE("Testing Cholesky Decomposition"){
-    SUBCASE("Square Matrix Decomposition"){
+    SUBCASE("Square Matrix Decomposition (5x5)"){
+        srand(100);
+        std::vector<float> data;
+        MPS::Matrix* source_mat = MPS::Matrix::alloc();source_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 5, 20, MPS::MPSDataTypeFloat32));
+        MPS::Matrix* source_psd = MPS::Matrix::alloc();source_psd->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 5, 20, MPS::MPSDataTypeFloat32));
+        MPS::Matrix* result_mat = MPS::Matrix::alloc();result_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 5, 20, MPS::MPSDataTypeFloat32));
+        MPS::Matrix* pivot_indices = MPS::Matrix::alloc();pivot_indices->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(1, 5, 20, MPS::MPSDataTypeUInt32));
 
-    }
-    SUBCASE("Rectangular Matrix Decomposition"){
 
+        MTL::CommandQueue* cmdQueue = device->newCommandQueue();
+        MTL::CommandBuffer* psdGenCmdBuf = cmdQueue->commandBuffer();
+        MPS::MatrixMultiplication* mps_matmul = MPS::MatrixMultiplication::alloc();mps_matmul->initWithDevice(device,false, true, 5, 5, 5, 1.0, 0.0);
+        generateRandomFloatData(source_mat->data());
+        mps_matmul->encodeToCommandBuffer(psdGenCmdBuf, source_mat, source_mat, source_psd);
+        psdGenCmdBuf->commit();
+        psdGenCmdBuf->waitUntilCompleted();
+        MPS::MatrixDecompositionCholesky* cholesky_decmp = MPS::MatrixDecompositionCholesky::alloc();
+        MTL::CommandBuffer* cmdBuffer = cmdQueue->commandBuffer();
+        cholesky_decmp->initWithDevice(device, true, 5);
+        cholesky_decmp->encodeToCommandBuffer(cmdBuffer, source_psd, result_mat, nullptr);
+        cmdBuffer->commit();
+        cmdBuffer->waitUntilCompleted();
+        removeElementsAboveDiagonal(((float *)result_mat->data()->contents()), 5, 5);
+
+
+        MPS::Matrix* reconstructed_mat = MPS::Matrix::alloc();reconstructed_mat->initWithDevice(device, MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 5, 20, MPS::MPSDataTypeFloat32));
+        MTL::CommandBuffer* reconstructCmdBuf = cmdQueue->commandBuffer();
+        MPS::MatrixMultiplication* reconstruct_matmul = MPS::MatrixMultiplication::alloc();reconstruct_matmul->initWithDevice(device, false, true, 5, 5, 5, 1.0, 0.0);
+        reconstruct_matmul->encodeToCommandBuffer(reconstructCmdBuf, result_mat, result_mat, reconstructed_mat);
+        reconstructCmdBuf->commit();
+        reconstructCmdBuf->waitUntilCompleted();
+        
+        CHECK(compareBuffers(source_psd->data(), reconstructed_mat->data()));
+        // Interesting how metal's cholesky doesn't actually remove elements above the diagonal on its own.
     }
 }
--- tests/MPSMatrixFindTopK_test.cpp
+++ tests/MPSMatrixFindTopK_test.cpp
@@ -2,6 +2,29 @@
 #include "../metal-cpp/MetalPerformanceShaders/MPSMatrix/MPSMatrixFindTopK.hpp"
 
 MTL::Device* dev = MTL::CreateSystemDefaultDevice();
+std::vector<float> not_in_place_sort(std::vector<float> original) {
+    std::sort(original.begin(), original.end(), std::greater<float>());
+    return original;
+}
+bool verifyTopK(float* inputs, int numberOfTopVals, float* result, int numRows, int numCols){
+    float* calc_res = new float[numRows*numberOfTopVals];
+    for(int i=0; i<numRows; i++){
+        std::vector<float> rowVec;
+        for(int j=0; j<numCols;j++){
+            rowVec.push_back(inputs[i*numCols + j]);
+        }
+        std::vector<float> sorted_row = not_in_place_sort(rowVec);
+        for(int k=0; k<numberOfTopVals;k++){
+            calc_res[i*numberOfTopVals + k] = sorted_row[k];
+        }
+    }
+    for(int i=0; i<numRows*numberOfTopVals;i++){
+        if(!areEqual(calc_res[i], result[i])){
+            return false;
+        }
+    }
+    return true;
+}
 TEST_CASE("Creating the MPSMatrixFindTopK Object"){
     MPS::MatrixFindTopK* findtopk_obj = MPS::MatrixFindTopK::alloc();
     findtopk_obj->initWithDevice(dev, 4);
@@ -9,27 +32,18 @@ TEST_CASE("Creating the MPSMatrixFindTopK Object"){
 }
 TEST_CASE("Finding the top K values"){
     MPS::MatrixFindTopK* findtopk_obj = MPS::MatrixFindTopK::alloc();
-    findtopk_obj->initWithDevice(dev, 1);
+    findtopk_obj->initWithDevice(dev, 2);
     MPS::MatrixDescriptor* mat_des = MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 5, 20, MPS::MPSDataTypeFloat32);
     MPS::MatrixDescriptor* index_matrix_des = MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 2, 8, MPS::MPSDataTypeUInt32);
     MPS::MatrixDescriptor* val_matrix_des = MPS::MatrixDescriptor::matrixDescriptorWithRows(5, 2, 8, MPS::MPSDataTypeFloat32);
     MPS::Matrix* matrix = MPS::Matrix::alloc(); matrix->initWithDevice(dev, mat_des);
-
     MPS::Matrix* index_matrix = MPS::Matrix::alloc(); index_matrix->initWithDevice(dev, index_matrix_des);
-
     MPS::Matrix* val_matrix = MPS::Matrix::alloc(); val_matrix->initWithDevice(dev, val_matrix_des);
-    //assert(matrix->dataType() == val_matrix->dataType());
     generateRandomFloatData(matrix->data());
-    // generateRandomFloatData(val_matrix->data());
-    std::cout<<"Mat1 datatype:"<<matrix->dataType()<<std::endl;
-    std::cout<<"Mat2 datatype:"<<val_matrix->dataType()<<std::endl;
-    std::cout<<"Mat3 datatype:"<<index_matrix->dataType()<<std::endl;
     MTL::CommandQueue* cmdQueue = dev->newCommandQueue();
     MTL::CommandBuffer* cmdBuffer = cmdQueue->commandBuffer();
     findtopk_obj->encodeToCommandBuffer(cmdBuffer, matrix, index_matrix, val_matrix);
     cmdBuffer->commit();
     cmdBuffer->waitUntilCompleted();
-    printMTLBuffer(matrix->data(), "Matrix Data");
-    printMTLBufferInt32(index_matrix->data(), "Index data");
-    printMTLBuffer(val_matrix->data(), "Value data");
+    CHECK(verifyTopK((float *)(matrix->data()->contents()), 2,(float *)(val_matrix->data()->contents()) , 5, 5));
 }
